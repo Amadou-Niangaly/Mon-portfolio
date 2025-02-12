7cfe1047bd6a4b455ce6c4631cb92cde703a4aca
Amadou Niangaly <amadouniangaly16@.com>
2025-02-12 19:05:07 +0000

    commit avant le deploiement

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,16 @@
 import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  showMoreInfo=false;
+  toggleInfo(){
+    this.showMoreInfo = !this.showMoreInfo;
+  }
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm = new FormGroup({
+    nomComplet: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    tel: new FormControl('', [Validators.pattern('^[0-9]{8,15}$')]),
+    sujet: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required]),
+  });
+
+  ngOnInit(): void {}
+
+  async onSubmit() {
+    if (this.contactForm.valid) {
+      const formData = this.contactForm.value;
+
+      const emailParams = {
+        nomComplet: formData.nomComplet,
+        email: formData.email,
+        tel: formData.tel,
+        sujet: formData.sujet,
+        message: formData.message,
+      };
+
+      try {
+        const response = await emailjs.send(
+          'service_frxxyg9',    // Remplace par ton Service ID EmailJS
+          'template_ksf27il',   // Remplace par ton Template ID EmailJS
+          emailParams,
+          'BjXGHG7-K8eFYsEMJ'   // Remplace par ta Public Key EmailJS
+        );
+
+        console.log('Email envoyé avec succès!', response.status, response.text);
+        alert('Votre message a été envoyé avec succès!');
+        this.contactForm.reset();
+      } catch (error) {
+        console.error('Erreur lors de l’envoi de l’email:', error);
+        alert('Une erreur est survenue lors de l’envoi du message.');
+      }
+    } else {
+      alert('Veuillez remplir correctement tous les champs.');
+    }
+  }
 
 }
